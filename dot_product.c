e310ac3eaac334b6520d9f04f8429c441b4bf502
remy <remyciterin@gmail.com>
2025-12-18 17:38:56 +0100

    wip

--- dot_product.c
+++ dot_product.c
@@ -17,11 +17,11 @@
 
 
 static inline void Write_Vector(XMk_dot_product* cfg, int offset, int* vector, int size) {
-  //XMk_dot_product_Write_Scratchpad_Words(cfg, offset, (word_type*)(vector), size);
   assert(offset % 4 == 0);
   assert(size % 4 == 0);
 
   printf("Test1 offset: %d size: %d vector%x\n", offset, size, vector);
+  fflush(stdout);
 
   XMk_dot_product_Write_Scratchpad_0_Words(cfg, offset / 4, (word_type*)vector[0*size/4], size / 4);
   XMk_dot_product_Write_Scratchpad_1_Words(cfg, offset / 4, (word_type*)vector[1*size/4], size / 4);
