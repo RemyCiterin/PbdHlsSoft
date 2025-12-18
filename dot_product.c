d285a933e5c51cf9fb2ad9699307e23dada5f10d
remy <remyciterin@gmail.com>
2025-12-18 15:40:04 +0100

    wip

--- dot_product.c
+++ dot_product.c
@@ -84,7 +84,7 @@ int main() {
   int A[SIZE];
 
   float t1 = dtime();
-  for (int i=0; i < SIZE; i++) A[i] = 1 * 6553;
+  for (int i=0; i < SIZE; i++) A[i] = 65536;
   Write_Vector(&ex, 0, &A[0], SIZE);
 
   int ret;
@@ -96,7 +96,7 @@ int main() {
   int B[COPIES][SIZE];
   for (int j=0; j < 100; j++) {
   for (int iter=0; iter < COPIES; iter++) {
-    for (int i=0; i < SIZE; i++) B[iter][i] = 1; // 65536 * (iter * SIZE+i);
+    for (int i=0; i < SIZE; i++) B[iter][i] = 65536; // 65536 * (iter * SIZE+i);
 
     copy_time -= dtime();
     Write_Vector(&ex, (1+iter) * SIZE, &B[iter][0], SIZE);
